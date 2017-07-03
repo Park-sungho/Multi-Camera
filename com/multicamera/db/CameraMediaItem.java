43bd51d66b52bc08361b1434b81a4c3fddea5d5a
Sungho <skyreon@gmail.com>
2017-07-03 18:47:21 +0900

    Create CameraMediaItem.java

---
+++ com/multicamera/db/CameraMediaItem.java
@@ -0,0 +1,12 @@
+/**
+ * This program is free software:
+ * Added by park Sung-ho */
+package com.multicamera.db;
+
+public class CameraMediaItem {
+  public String mIdx;
+  public String mTitle;
+  public String mUrl;
+  public String mContentName;
+  public long mModifyDate;
+}
